8cc90cab544eba404f1b7e3df85068a8547c181c
somenctg <somensingha06@gmail.com>
2021-02-09 17:50:43 +0600

    added

---
+++ app.js
@@ -0,0 +1,30 @@
+const searchMenus = () => {
+  const searchText = document.getElementById('search-field').value;
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displayMenus(meals.data))
+}
+const displayMenus = meals => {
+  const menuContainer = document.getElementById('meal-container');
+  menuContainer.innerHTML = '';
+
+  meals.forEach(meal => {
+    const menuDiv = document.createElement('div');
+    menuDiv.className = 'single-result';
+    menuDiv.innerHTML = `
+       <div class="meal-item">
+          
+            <img src="${meal.strMealThumb}" alt="food">
+            <h3>${meal.strMeal}</h3>
+          </div>
+
+
+      
+       `;
+       menuContainer.appendChild(menuDiv);
+       
+
+
+  });
+}
